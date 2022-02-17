33d835e9bba36d6f6c0d1bfb76f2ac35c5f739ce
dshedd25 <99275733+dshedd25@users.noreply.github.com>
2022-02-17 09:35:40 -0700

    Create 3.2_Controling_digital_outputs.ino

---
+++ 3.2_Controling_digital_outputs.ino
@@ -0,0 +1,38 @@
+/*
+
+*/
+
+void setup() {
+     pinMode(7, OUTPUT);
+     digitalWrite(8, HIGH);
+    delay(200);
+    digitalWrite(8, LOW);
+    delay(200);
+    digitalWrite(8, HIGH);
+    delay(200);
+    digitalWrite(8, LOW);
+    delay(1000);
+    digitalWrite(7, HIGH);
+    delay(200);
+    digitalWrite(7, LOW);
+    delay(200);
+    digitalWrite(7, HIGH);
+    delay(200);
+    digitalWrite(7, LOW);
+    delay(3000);
+    
+    
+}
+
+void loop() {
+  digitalWrite(8, HIGH);
+    delay(200);
+    digitalWrite(8, LOW);
+    delay(200);
+    digitalWrite(7, HIGH);
+    delay(200);
+    digitalWrite(7, LOW);
+    
+   
+    
+}
